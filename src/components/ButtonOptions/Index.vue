e3c575ab507af8f577cee6cd6762c30163c96601
sinana <sinana@test.com>
2023-08-23 18:16:02 +0800

    canvas 区域点击事件

--- src/components/ButtonOptions/Index.vue
+++ src/components/ButtonOptions/Index.vue
@@ -10,6 +10,7 @@ export default {
       isCoreMenu: true, // 是否展示中心按钮
       roundRadius: 50, // 中心圆半径
       radiusDistance: 3, //半径的长度比 （中心圆半径 = 大圆半径 * radiusDistance）
+      arrs: ["上", "下", "左", "右"],
     };
   },
   mounted() {
@@ -17,97 +18,150 @@ export default {
   },
   methods: {
     onDraw: function () {
+      const arr = this.arrs;
       const canvas = document.querySelector("#ctx");
       // canvas.style.border = "1px solid blue";
       const ctx = canvas.getContext("2d");
-      let drawArr = [];
       //圆弧的份数
       const num = 4;
       //一个圆弧对应的弧度
       const angle = (Math.PI * 2) / num;
-      console.log("angle", angle, angle * (180 / Math.PI));
       const coreX = this.canvasW / 2; // 圆心坐标
       const coreY = this.canvasH / 2;
-      this.roundRadius = coreX / 2.5;
+      const radiusM = this.canvasW / 2.1;
+      const roundRadius = coreX / 2.5;
       //随机颜色
-      let star = coreX;
+      let star = 0;
       let end = angle;
-      for (let i = 0; i < num; i++) {
-        drawArr.push({ paintP: this.paintP, star, end });
-        star = end;
-        end = end + angle;
-      }
-      drawArr.push({ paintCircle: this.paintCircle });
-      drawArr.forEach((item) => {
-        if (item["paintCircle"]) {
-          item["paintCircle"](ctx, coreX, coreY, star, end);
-        } else {
-          item["paintP"](
-            ctx,
-            coreX,
-            coreY,
-            this.canvasW / 2.1,
-            item.star,
-            item.end
-          );
+      paintP();
+      paintCircle();
+
+      function paintP() {
+        console.log("arr", arr);
+        for (let i = 0; i < num; i++) {
+          ctx.save();
+          ctx.beginPath();
+          ctx.moveTo(coreX, coreY);
+          ctx.arc(coreX, coreY, radiusM, star, end);
+          ctx.closePath();
+          ctx.fillStyle = "#ccc";
+          ctx.font = "12px scans-serif";
+          // ctx.fillText(arr[i], 120, 120);
+          ctx.stroke();
+          ctx.fill();
+          ctx.restore();
+          star = end;
+          end = end + angle;
         }
-      });
-      console.log("drawArr0", drawArr);
+      }
+      function paintCircle() {
+        ctx.beginPath();
+        ctx.arc(coreX, coreY, roundRadius, 0, Math.PI * 2, true);
+        ctx.fillStyle = "#ccc";
+        // ctx.fillText("中心", 140, 140);
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+      }
+
+      function redrawPaintP(index) {
+        for (let i = 0; i < num; i++) {
+          ctx.save();
+          ctx.beginPath();
+          ctx.moveTo(coreX, coreY);
+          ctx.arc(coreX, coreY, radiusM, star, end);
+          ctx.closePath();
+          if (index === i) {
+            console.log("i", i);
+            ctx.fillStyle = "#ff9000";
+          } else {
+            ctx.fillStyle = "ccc";
+          }
+          ctx.font = "12px scans-serif";
+          ctx.stroke();
+          ctx.fill();
+          ctx.restore();
+          star = end;
+          end = end + angle;
+        }
+        paintCircle(); // 保证中心圆是是最后一个绘制（重要必须！！！）
+      }
+
+      function redrawPaintCircle() {
+        ctx.save();
+        ctx.beginPath();
+        ctx.arc(coreX, coreY, roundRadius, 0, Math.PI * 2, true);
+        ctx.stroke();
+        ctx.fillStyle = "#ff9000";
+        ctx.fill();
+        ctx.closePath();
+        ctx.restore();
+      }
+
       canvas.onclick = function (e) {
         const event = e || event;
         const x = e.clientX - canvas.offsetLeft; //获取点击后x的坐标
         const y = e.clientY - canvas.offsetTop; //获取点击后y的坐标
-        drawArr.forEach((item) => {
-          // item();
-          if (item["paintCircle"]) {
-            item["paintCircle"](ctx, coreX, coreY, star, end);
-            const isTure = ctx.isPointInPath(x, y);
-            console.log("isTure0000", isTure);
-            if (isTure) {
-              return;
-            }
-          } else {
-            item["paintP"](
-              ctx,
-              coreX,
-              coreY,
-              this.canvasW / 2.1,
-              item.star,
-              item.end
-            );
-            const isTure = ctx.isPointInPath(x, y);
-            console.log("isTure111", isTure);
-            if (isTure) {
-              return;
-            }
-          }
-        });
+        // const isTure = ctx.isPointInPath(x, y); // canvas事件中 该方法只会判断最后一层
+        const curClickPointR =
+          Math.pow(x - coreX, 2) + Math.pow(y - coreY, 2) <
+          Math.pow(radiusM, 2);
+        const curR =
+          Math.pow(x - coreX, 2) + Math.pow(y - coreY, 2) <
+          Math.pow(roundRadius, 2);
+        if (curR) {
+          paintP();
+          redrawPaintCircle();
+          console.log("在中心圆中");
+          return;
+        }
+        if (
+          curClickPointR &&
+          y > coreY &&
+          y - coreY < Math.tan(angle * 1) * (x - coreX)
+        ) {
+          console.log("在扇形0内");
+          redrawPaintP(0);
+          return;
+        }
+        if (
+          curClickPointR &&
+          y - coreY > Math.tan(angle * 1) * (x - coreX) &&
+          y - coreY > Math.tan(angle * 2) * (x - coreX)
+        ) {
+          console.log("在扇形1内");
+          redrawPaintP(1);
+          return;
+        }
+        if (
+          curClickPointR &&
+          y < coreY &&
+          y - coreY < Math.tan(angle * 2) * (x - coreX) &&
+          y - coreY > Math.tan(angle * 3) * (x - coreX)
+        ) {
+          console.log("在扇形2内");
+          redrawPaintP(2);
+          return;
+        }
+        if (
+          curClickPointR &&
+          y < coreY &&
+          y - coreY < Math.tan(angle * 4) * (x - coreX)
+        ) {
+          console.log(
+            "在扇形3内",
+            y - coreY < Math.tan(angle * 4) * (x - coreX)
+          );
+          redrawPaintP(3);
+          return;
+        }
       };
-    },
-    paintCircle: function (ctx, coreX, coreY, startAngle, endAngle) {
-      ctx.beginPath();
-      ctx.arc(coreX, coreY, this.roundRadius, 0, Math.PI * 2, true);
-      ctx.stroke();
-      ctx.fillStyle = this.getRandomColor();
-      ctx.fill();
-      ctx.closePath();
-    },
-    paintP: function (ctx, x, y, r, startAngle, endAngle) {
-      ctx.save();
-      ctx.beginPath();
-      ctx.moveTo(x, y);
-      ctx.arc(x, y, r, startAngle, endAngle);
-      ctx.closePath();
-      ctx.fillStyle = "#ccc";
-      ctx.stroke();
-      ctx.fill();
-      ctx.restore();
     },
     getRandomColor: function () {
       const r = parseInt(Math.random() * 256);
       const g = parseInt(Math.random() * 256);
       const b = parseInt(Math.random() * 256);
-      return "rgb(" + r + "," + g + "," + g + ")";
+      return "rgb(" + r + "," + g + "," + b + ")";
     },
   },
 };
--- src/components/cssTest/Index.vue
+++ src/components/cssTest/Index.vue
@@ -1,38 +1,48 @@
 <template>
-  <div></div>
+  <div class="cc">
+    <div class="ccD div1">1</div>
+    <div class="ccD div2">2</div>
+    <div class="ccD div3">3</div>
+    <div class="ccD div4">4</div>
+  </div>
 </template>
 <script>
 export default {};
 </script>
 <style lang="scss">
-div {
-  position: absolute;
-  top: 0;
-  left: 0;
+.ccD {
   width: 100px;
   height: 100px;
-  border-radius: 50%;
-  background: #0cf;
-  animation: horizontal 2.6s infinite linear alternate,
-    vertical 1.8s infinite linear alternate;
-  animation-composition: accumulate;
+  border: 1px solid skyblue;
 }
+// div {
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   width: 100px;
+//   height: 100px;
+//   border-radius: 50%;
+//   background: #0cf;
+//   animation: horizontal 2.6s infinite linear alternate,
+//     vertical 1.8s infinite linear alternate;
+//   animation-composition: accumulate;
+// }
 
-@keyframes horizontal {
-  from {
-    left: 0;
-  }
-  to {
-    left: calc(100% - 100px);
-  }
-}
+// @keyframes horizontal {
+//   from {
+//     left: 0;
+//   }
+//   to {
+//     left: calc(100% - 100px);
+//   }
+// }
 
-@keyframes vertical {
-  from {
-    top: 0;
-  }
-  to {
-    top: calc(100% - 100px);
-  }
-}
+// @keyframes vertical {
+//   from {
+//     top: 0;
+//   }
+//   to {
+//     top: calc(100% - 100px);
+//   }
+// }
 </style>
