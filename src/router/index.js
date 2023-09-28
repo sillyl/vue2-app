c3e0b83e8d1c053dddc665f520f8efc5036c86d5
sinana <sinana@test.com>
2023-09-28 17:27:23 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -82,7 +82,7 @@ const routes = [
   {
     path: "/TestLaochenBili",
     name: "TestLaochenBili",
-    component: () => import(/* webpackChunkName: "webrtcSteamer" */ "@/views/Three/TestLaochenBili/Index1.vue"),
+    component: () => import(/* webpackChunkName: "webrtcSteamer" */ "@/views/Three/TestLaochenBili/MeshBasicMaterial.vue"),
   },
 
   {
